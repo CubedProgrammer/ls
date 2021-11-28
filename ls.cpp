b62c6569b112fd53da6ad9cff25a16d9a2c3e7b7
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-11-28 18:57:22 -0500

    Entries displayed column by column.

--- ls.cpp
+++ ls.cpp
@@ -3,6 +3,7 @@
 #include"size_and_colours.hpp"
 
 //using std::filesystem::absolute;
+using std::filesystem::directory_entry;
 using std::filesystem::directory_iterator;
 using std::filesystem::path;
 using std::cout;
@@ -19,20 +20,31 @@ int main(int argl, char* argv[])
     if (currstr.back() == '\\')
         currstr = currstr.substr(0, currstr.size() - 1);
     size_t maxi = 0;
-    for (const auto& cont : directory_iterator(curr))
-        maxi = std::max(maxi, cont.path().string().size());
-    maxi -= currstr.size() + 1;
-    size_t cols = consz.first / (maxi + 2), rm;
-    size_t colcnt = 0;
-    std::string tmp;
+    std::vector<directory_entry>dir_ens;
     for (const auto& cont : directory_iterator(curr))
     {
+        maxi = std::max(maxi, cont.path().string().size());
+        dir_ens.push_back(cont);
+    }
+    maxi -= currstr.size() + 1;
+    size_t cols = consz.first / (maxi + 2), rm;
+    size_t colcnt = 0, last_row_cnt = dir_ens.size() % cols;
+    size_t rcnt = dir_ens.size() / cols;
+    std::string tmp;
+    //for (const auto& cont : directory_iterator(curr))
+    for(size_t i = -1, ps = 0; ps < dir_ens.size(); ++ps, i+= rcnt)
+    {
+        if (colcnt == 0)
+            i = (i + 1) % dir_ens.size();
+        const auto& cont = dir_ens[i];
         tmp = cont.path().string().substr(currstr.size() + 1);
         if (cont.is_regular_file())
             cout << reset_console;
         else if (cont.is_directory())
             cout << blue_foreground;
         cout << tmp;
+        if (colcnt < last_row_cnt)
+            ++i;
         if (colcnt == cols - 1)
         {
             cout << '\n';
