c49da4b56c9ce807a54d20ecd806e3b73c3cfdcc
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2024-09-23 17:36:19 +0000

    Fixed empty directory bug.

--- ls.cpp
+++ ls.cpp
@@ -15,38 +15,42 @@ using std::views::transform;
 using std::cout;
 using std::endl;
 
+std::array<std::string, 3>execute_exts{ ".ps1", ".bat", ".exe" };
 std::array<std::string, 5>archive_exts{ ".7z" , ".zip" , ".tar" , ".tar.gz" , ".jar" };
 std::array<std::string, 5>picture_exts{ ".bmp", ".gif", ".jpg", ".png", ".svg" };
 
 void print_entry(const path& cont)
 {
+    using std::ranges::contains;
+    bool reset = true;
     if (is_regular_file(cont))
     {
         std::string ext = cont.extension().string();
-        if (ext == ".psh" || ext == ".bat" || ext == ".exe")
+        if (contains(execute_exts, ext))
             cout << green_foreground;
-        else if(ext == ".lnk")
+        else if (ext == ".lnk")
             cout << cyan_foreground;
-        else if (std::find(archive_exts.begin(), archive_exts.end(), ext) != archive_exts.end())
+        else if (contains(archive_exts, ext))
             cout << red_foreground;
-        else if (std::find(picture_exts.begin(), picture_exts.end(), ext) != picture_exts.end())
+        else if (contains(picture_exts, ext))
             cout << magenta_foreground;
         else
-            cout << reset_console;
+            reset = false;
     }
     else if (is_symlink(cont))
         cout << cyan_foreground;
     else if (is_directory(cont))
         cout << blue_foreground;
     cout << cont.string();
+    if (reset)
+        cout << reset_console;
 }
 
-void display_entries(std::pair<int, int>consz, bool bycol, const std::vector<directory_entry>& dens)
+void display_entries(std::pair<int, int>consz, bool bycol, const std::vector<std::string>& ens)
 {
     using namespace std::views;
-    auto ens = dens | transform([](const directory_entry& entry) {return entry.path().filename().string(); });
-    size_t maxi = (*max_element(ens, std::ranges::less{}, [](const std::string& entry) {return entry.size(); })).size();
-    size_t cols = consz.first / (maxi + 2), rm;
+    size_t maxi = (*max_element(ens, std::ranges::less{}, &std::string::size)).size();
+    size_t cols = consz.first / (maxi + 2);
     size_t last_row_cnt = ens.size() % cols;
     size_t rcnt = ens.size() / cols;
     std::string tmp;
@@ -120,19 +124,21 @@ int main_pp(std::vector<std::string>& args)
     path curr = currstr;
     if (currstr.back() == '\\')
         currstr = currstr.substr(0, currstr.size() - 1);
-    std::vector<directory_entry>dir_ens;
+    std::vector<std::string>dir_ens;
     if (dots)
     {
-        dir_ens.push_back(directory_entry{ currstr + "\\." });
-        dir_ens.push_back(directory_entry{ currstr + "\\.." });
+        dir_ens.push_back({ currstr + "\\." });
+        dir_ens.push_back({ currstr + "\\.." });
     }
+    auto direntstr = [](const directory_entry& en) { return en.path().string(); };
     if (recurse)
-        copy(recursive_directory_iterator(curr), std::back_inserter(dir_ens));
+        dir_ens.append_range(transform(recursive_directory_iterator(curr), direntstr));
     else
-        copy(directory_iterator(curr), std::back_inserter(dir_ens));
+        dir_ens.append_range(transform(directory_iterator(curr), direntstr));
     if (rev)
         std::ranges::reverse(dir_ens);
-    display_entries(consz, bycol, dir_ens);
+    if (!std::ranges::empty(dir_ens))
+        display_entries(consz, bycol, dir_ens);
     return 0;
 }
 
